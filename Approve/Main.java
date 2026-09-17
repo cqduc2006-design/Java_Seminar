83c71230ef1050ef9bbd8c8d200ce8e0590d563a
cqduc2006-design <cqduc2006@gmail.com>
2026-09-18 01:14:29 +0700

    Update Main.java

--- Approve/Main.java
+++ Approve/Main.java
@@ -2,7 +2,7 @@ package Approve;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isApproved = Approve.approveForLoan(4, false, true, true, true, true, false);
+        boolean isApproved = Approve.approveForLoan(4, false, false, false, false, false, false);
 
         if (isApproved) {
             System.out.println("Loan Approved");
