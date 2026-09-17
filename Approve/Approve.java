ee757393ee3d38e6c81c79b8efc3eb61138a4c0b
cqduc2006-design <cqduc2006@gmail.com>
2026-09-18 01:12:12 +0700

    Update Approve.java

--- Approve/Approve.java
+++ Approve/Approve.java
@@ -2,16 +2,21 @@ package Approve;
 
 public class Approve {
 
-    public static boolean approveForLoan(int yearWorking, boolean badLoan, boolean isStartup, 
-                                         boolean startupFund, boolean bigFigure, 
-                                         boolean vipCustomer, boolean isBlacklisted) {
+    public static boolean approveForLoan(int yearWorking, boolean badLoan, boolean isStartup, boolean startupFund, 
+                                         boolean bigFigure, boolean vipCustomer, boolean isBlacklisted) {
         
-        if (isBlacklisted) {
-            return false;
+        if (!isBlacklisted) {
+            if (yearWorking > 3 && !badLoan) {
+                return true;
+            } else if (isStartup && startupFund) {
+                return true;
+            } else if (vipCustomer || bigFigure) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        return (yearWorking > 3 && !badLoan) 
-               || (isStartup && startupFund) 
-               || (vipCustomer || bigFigure);
+        return false;
     }
 }
